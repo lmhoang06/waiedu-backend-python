4fa86a35a6f11676b4990bdd61aee5986da087d2
Lê Minh Hoàng <minhhoanghsftg@gmail.com>
2025-04-21 15:42:18 +0000

    prepare for heroku deploy

--- app/extensions.py
+++ app/extensions.py
@@ -2,11 +2,18 @@ import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 import dotenv
+import json
+import base64
 
 dotenv.load_dotenv()
 
 def initialize_firebase(app):
-    cred_path = os.path.join(os.path.dirname(__file__), os.getenv('FIREBASE_CRED_PATH'))
-    cred = credentials.Certificate(cred_path)
+    cred = credentials.Certificate(
+        json.loads(
+            base64.b64decode(
+                os.getenv("FIREBASE_CRED")
+            ).decode("utf-8")
+        )
+    )
     firebase_admin.initialize_app(cred)
     app.firestore_db = firestore.client()
--- wsgi.py
+++ wsgi.py
@@ -5,4 +5,4 @@ application = create_app()
 
 # This allows the file to be run directly using `python wsgi.py`
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", debug=True)
+    application.run(debug=True)
