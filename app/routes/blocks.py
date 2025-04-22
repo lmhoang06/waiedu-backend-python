823e8aacc578a3ca3b6e2381cd3427ec05294a1d
Lê Minh Hoàng <minhhoanghsftg@gmail.com>
2025-04-22 12:59:50 +0000

    add fields allowed to blocks

--- app/routes/blocks.py
+++ app/routes/blocks.py
@@ -6,7 +6,7 @@ block_bp = Blueprint('block', __name__, url_prefix='/blocks')
 
 # Define allowed fields that will be returned in the response
 # This can be modified later as needed
-ALLOWED_FIELDS = ['tenKhoi', 'loaiKhoi', 'blobUrl', 'canNang', 'id', 'donViCanNang', 'kichThuoc']
+ALLOWED_FIELDS = ['tenKhoi', 'loaiKhoi', 'blobUrl', 'canNang', 'id', 'donViCanNang', 'kichThuoc', 'chatLieu', 'mauSac', 'moTa']
 
 @block_bp.route('/', methods=['GET'])
 def get_all_blocks():
